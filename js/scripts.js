6eb5080f8f34eb84987272d2d9d7a934ba36badb
Patryk Uran <trykpa@gmail.com>
2017-04-22 22:08:31 +0200

    fix missing semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
 
 if (value > 0) {
-	console.log("Wynik dodatni")
+	console.log("Wynik dodatni");
 } else if (value < 0) {
-	console.log("Wynik ujemny")
+	console.log("Wynik ujemny");
 } else {
-	console.log("Wynik wynosi 0")
+	console.log("Wynik wynosi 0");
 }
